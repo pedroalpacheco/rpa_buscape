b49bb008302807b5602df6f5b712e5188b4bb43f
papacheco <pedro.pacheco.a@gmail.com>
2021-09-14 15:47:40 -0300

    Relatorios melhorados

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const timestamp = new Date().getTime();
     });
     const page = await browser.newPage();
 
-    await page.goto(siteAlvo, {waitUntil:'networkidle0'});
+    await page.goto(siteAlvo, {waitUntil:'networkidle2'});
     await page.waitForSelector('.button-filter');
     await page.click('.button-filter');
     await page.waitForSelector('#__next > div.SearchPage_SearchPageContainer__xpPgJ.container-lg > div > div.Filters_Filters__3C2Tt.Filters_visible__1o3ra.col-xl-3.col-lg-3 > div.Filters_Filters__Content__1Johw > aside > div:nth-child(5) > ul > li:nth-child(3) > a > div > div > span.ais-RefinementList-label');
@@ -30,11 +30,12 @@ const timestamp = new Date().getTime();
 
     await page.waitForTimeout('5000')
     await page.evaluate(() => {
-        window.scrollBy(0, window.innerHeight);
+        window.scrollBy(0, 5000);
     });
-    await page.waitForTimeout('5000')
+    await page.waitForTimeout('35000')
     await page.screenshot({ path: `Relatorio-${timestamp}.png`, fullPage: true })
     await page.pdf({ path: `Relatorio-${timestamp}.pdf`, format: 'a4' });
+    await console.log(`Relatorios criados: Relatorio-${timestamp}.png / Relatorio-${timestamp}.pdf`)
 
 
     await browser.close();
