6e15f2b9d8e23a1c3daeff45877552215a3c8d3f
papacheco <pedro.pacheco.a@gmail.com>
2021-09-27 13:15:21 -0300

    Ajustando bootstrap

--- rpa_BP-web/src/app.js
+++ rpa_BP-web/src/app.js
@@ -1,9 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const path = require('path');
+
 const port = 3006
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    //res.send('Hello World!')
+    res.sendFile(path.join(__dirname + '/index.html'));
 })
 
 app.listen(port, () => {
